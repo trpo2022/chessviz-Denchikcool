ce49a554460ebbe1c0703876f5d55359da49bd3b
Денис Кузьменок <kuzmenok.den@yandex.ru>
2022-05-11 13:22:29 +0700

    Fixed at lesson

--- test/mytests.c
+++ test/mytests.c
@@ -1,15 +1,5 @@
 #include <ctest.h>
 #include <libchess/check_move.h>
-#include <libchess/enum.h>
-#include <libchess/fill_table.h>
-#include <libchess/find_position_black.h>
-#include <libchess/find_position_white.h>
-#include <libchess/input.h>
-#include <libchess/move.h>
-#include <libchess/print_table.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 CTEST(ctest, write1)
 {
